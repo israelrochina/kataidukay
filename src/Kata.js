2d7d14bff7b6b9b230184fe09b0aae87c462df4e
wellington <wellirochi89@gmail.com>
2021-03-15 09:13:44 -0500

    rrecorido caracol para n*m

--- src/Kata.js
+++ src/Kata.js
@@ -8,11 +8,13 @@ class Kata extends Component {
     this.state = {};
   }
 
-
+componentWillMount(){
+  this.generateArray(3,5);
+}
   generateArray(rowNumber, columnsNumber) {
     /* console.log("rowNumber:" + rowNumber)
     console.log("columnsNumber:" + columnsNumber) */
-    let printerArray=[];
+  /*   let printerArray=[];
     let count=1;
     
     
@@ -24,16 +26,65 @@ class Kata extends Component {
       }
       printerArray.push(initRow)
     }
+    this.setState({printerArray:printerArray}); */
+
+    let count=1;
+    let printerArray=[];
+    for(let row=0;row<rowNumber;row++){
+      const initRow=Array.from({length:columnsNumber},()=>count++);
+      printerArray.push(initRow)
+    }
     this.setState({printerArray:printerArray});
 }
 
 
+loopArrayInSnail(){
+  // console.log('Array',this.state.printerArray)
+  const loopArray=this.state.printerArray;
+  const result=[]
+
+  const columNumber=loopArray[0].length-1;
+  const rowNumber=loopArray.length-1;
+
+
+console.log('ColNumber',columNumber)
+console.log('rowNumber',rowNumber)
+
+  for(let item=0;item<=columNumber;item++){
+    
+    result.push(loopArray[0][item]);
+  
+  }
+
+  for(let row=1;row<=rowNumber;row++){
+    result.push(loopArray[row][columNumber]);
+  }
+
+  for(let row=columNumber-1;row>=0;row--){
+    result.push(loopArray[rowNumber][row]);
+  
+  }
+
+  for(let up=0;up<=columNumber-1;up++){
+    result.push(loopArray[rowNumber-1][up]);
+  
+  }
+
+console.log('Array',result)
+this.setState({resultArray:result})
+}
+//loopArray[fila][columna]
 
 
     render() {
       return (
         <div className="Kata">
-          Trabajaremos el ejercicio aqui
+       {this.state.printerArray.map((row)=>{
+         return row.map((col)=>{
+           return <span>{col}</span>
+         });
+         
+       })}
         </div>  
       );
     }
--- src/specs/Kata.spec.js
+++ src/specs/Kata.spec.js
@@ -25,6 +25,38 @@ describe('Kata', () => {
         [11, 12, 13, 14, 15]
       ]);
     });
+
+
+    it('Este test deberia retornar la impresion de una matriz recorrida en forma de caracol', () => {
+      instance.state.printerArray=[
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ];
+
+      instance.loopArrayInSnail();
+
+      expect(instance.state.resultArray).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+
+    });
+
+    fit('Este test deberia retornar la impresion de una matriz recorrida en forma de caracol', () => {
+      instance.state.printerArray=[
+        [1, 2, 3, 4, 5, 6],
+        [7, 8, 9, 10, 11, 12],
+        [13, 14, 15, 16, 17, 18]
+      ];
+
+      instance.loopArrayInSnail();
+
+      expect(instance.state.resultArray).toEqual([1, 2, 3, 4, 5, 6, 12, 18, 17, 16, 15, 14, 13, 7, 8, 9,10, 11]);
+
+    });
+
+
+
   });
+
+
 });
 
